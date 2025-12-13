f3db25d2f8eb1dd9bc1bf9af74834a797976cee2
MAKSIM <mburdonos@mail.ru>
2025-12-13 23:43:23 +0300

    append information on main page

--- first_react_project/vite.config.ts
+++ first_react_project/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // слушать на всех интерфейсах
+    port: 5173,       // порт (по умолчанию 5173)
+    strictPort: true,  // не выбирать другой порт, если занят
+  },
   plugins: [
     react({
       babel: {
