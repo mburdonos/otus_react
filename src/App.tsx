47e674502ec3cbafd1796505d2d63dea3abe13d5
MAKSIM <mburdonos@mail.ru>
2026-02-25 16:33:01 +0300

    get base otus project form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 import { ThemeToggle } from './shared/ui/base_components/ThemeToggle/ThemeToggle';
 import LanguageSwitcher from './shared/ui/base_components/LanguageSwitcher/LanguageSwitcher';
+import { ProfileForm } from './shared/ui/forms';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
       </div>
       <LanguageSwitcher></LanguageSwitcher>
       <ThemeToggle></ThemeToggle>
+      <ProfileForm></ProfileForm>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
---
+++ src/shared/ui/forms/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,38 @@
+import React, { memo } from 'react';
+import cn from 'clsx';
+import type { ProfileFormProps } from './types';
+import { NameField } from './NameField';
+import s from './ProfileForm.sass';
+import { AboutField } from './AboutField';
+
+export const ProfileForm = memo<ProfileFormProps>(
+  ({ className, formManager, formElement, autoFocusElement, disabled }) => {
+    const { values, touched, errors, submitCount, handleBlur, handleSubmit, handleChange } = formManager;
+
+    return (
+      <form ref={formElement} onSubmit={handleSubmit} className={cn(s.root, className)}>
+        <NameField
+          autoFocusElement={autoFocusElement}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.name}
+          errors={errors.name}
+          submitCount={submitCount}
+          touched={touched.name}
+          disabled={disabled}
+        />
+        <AboutField
+          onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.about}
+          errors={errors.about}
+          submitCount={submitCount}
+          touched={touched.about}
+          disabled={disabled}
+        />
+      </form>
+    );
+  }
+);
+
+ProfileForm.displayName = 'ProfileForm';
