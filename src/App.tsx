818caf9ab5f06d6129985dad4933d08d2cc9857a
MAKSIM <mburdonos@mail.ru>
2026-03-13 16:27:20 +0300

    cleare main page

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,11 @@ import { ThemeToggle } from './shared/ui/base_components/ThemeToggle/ThemeToggle
 import LanguageSwitcher from './shared/ui/base_components/LanguageSwitcher/LanguageSwitcher';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <LanguageSwitcher></LanguageSwitcher>
       <ThemeToggle></ThemeToggle>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
