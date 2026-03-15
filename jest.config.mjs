55df6cac64f0dbd5b977145dcaa0b6685eb8624b
mburdonos <113140181+mburdonos@users.noreply.github.com>
2026-03-15 12:17:13 +0000

    Deploying to pages_routing from @ mburdonos/otus_react@cbac028058ecff5d7ce2370574fdc490bdbfc90b 🚀

--- jest.config.mjs
+++
@@ -1,9 +0,0 @@
-// jest.config.mjs
-export default {
-  testEnvironment: 'jest-environment-jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js|jsx)$'
-};
--- src/utils/random.ts
+++
@@ -1,5 +0,0 @@
-import { Random } from 'random-js';
-
-export const random = new Random();
-
-export const getRandomId = () => random.uuid4();
