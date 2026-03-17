5c3c83a153092d35dfaa360b384fd82584c9287e
MAKSIM <mburdonos@mail.ru>
2026-03-17 17:35:54 +0300

    fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/otus_react/',
   // base: '/', // или './' для деплоя в папку
   build: {
     outDir: 'dist', // папка сборки
