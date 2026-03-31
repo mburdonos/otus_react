84f2891471926f3557a1eb4ed9816a642da41281
MAKSIM <mburdonos@mail.ru>
2026-03-31 19:36:58 +0300

    append https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://19429ba06ff2.vps.myjino.ru/api';
+export const API_BASE_URL = 'https://19429ba06ff2.vps.myjino.ru/api';
 export const COMMAND_ID = 'bmp'; // Замените на ваш реальный commandId
 
 // Типы для API
